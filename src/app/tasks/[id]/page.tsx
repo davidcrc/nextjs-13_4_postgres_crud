73d011679ddcf666c27c4d5c041f437b6241cef6
davidcrc <david_drcr@hotmail.com>
2023-08-05 11:58:55 -0500

    feat: :sparkles: remove selected task

---
+++ src/app/tasks/[id]/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import FormPage from "../new/page";
+
+const Task = () => {
+  return <FormPage />;
+};
+
+export default Task;
--- src/app/tasks/new/page.tsx
+++ src/app/tasks/new/page.tsx
@@ -1,18 +1,18 @@
 "use client";
 
-import React from "react";
-import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
+import { useParams, useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 
 const FormPage = () => {
   const router = useRouter();
+  const params = useParams();
 
   const {
     register,
     handleSubmit,
     setFocus,
     reset,
-    watch,
     formState: { errors },
   } = useForm({
     defaultValues: {
@@ -44,12 +44,45 @@ const FormPage = () => {
     }
   });
 
+  const handleDelete = async () => {
+    console.log("deleteing");
+    try {
+      if (confirm("Are you sure you want to delete this task?")) {
+        await fetch(`/api/tasks/${params.id}`, {
+          method: "DELETE",
+        });
+      }
+
+      router.push("/");
+      router.refresh();
+    } catch (error) {
+      console.log("err", error);
+    }
+  };
+
+  useEffect(() => {
+    if (params.id) {
+      // TODO: reset form values with data
+    }
+  }, [params.id, reset]);
+
   return (
     <form
       onSubmit={onSubmit}
       className="h-[calc(100vh-7rem)] flex flex-col justify-center items-center max-w-xl mx-auto"
     >
-      <h1 className="font-bold text-3xl">Create Taks</h1>
+      <div className="flex gap-3 justify-between w-full ">
+        <h1 className="font-bold text-3xl">
+          {params.id ? "Update Tak" : "Create Tak"}
+        </h1>
+        <button
+          type="button"
+          className="bg-red-500 px-3 py-1 rounded-md"
+          onClick={handleDelete}
+        >
+          Delete
+        </button>
+      </div>
       <input
         {...register("title", {
           required: {
@@ -67,12 +100,15 @@ const FormPage = () => {
         className="bg-gray-800 border-2 w-full p-4 rounded-lg my-4"
       />
 
-      <button
-        className="bg-green-600 hover:bg-green-700 border-2 w-full px-4 py-2 rounded-lg my-4"
-        disabled={!!errors.title?.message}
-      >
-        Save
-      </button>
+      <div>
+        <button
+          type="submit"
+          className="bg-green-600 hover:bg-green-700 border-2 w-fit px-4 py-2 rounded-lg my-4 "
+          disabled={!!errors.title?.message}
+        >
+          Save
+        </button>
+      </div>
     </form>
   );
 };
