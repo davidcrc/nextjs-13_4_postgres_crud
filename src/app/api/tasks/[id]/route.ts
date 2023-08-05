27cb101fcd9a8f155df4bb0db231fb4a3061d18a
davidcrc <david_drcr@hotmail.com>
2023-08-05 11:05:04 -0500

    feat: delete tak

--- src/app/api/tasks/[id]/route.ts
+++ src/app/api/tasks/[id]/route.ts
@@ -23,10 +23,24 @@ export async function GET(resquest: NextRequest, { params }: TaskParams) {
   }
 }
 
-export function DELETE(resquest: NextRequest, { params }: TaskParams) {
-  return NextResponse.json({
-    message: "eliminando tarea",
-  });
+export async function DELETE(resquest: NextRequest, { params }: TaskParams) {
+  try {
+    const task = await prisma.task.delete({
+      where: {
+        id: Number(params.id),
+      },
+    });
+
+    if (!task) {
+      return NextResponse.json("Task not found", { status: 404 });
+    }
+
+    return NextResponse.json(task);
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(error.message, { status: 500 });
+    }
+  }
 }
 
 export async function PUT(resquest: NextRequest, { params }: TaskParams) {
